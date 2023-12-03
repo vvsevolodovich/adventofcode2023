77b512d529890bdeef39aa3e4aa0db2340bf8113
Vladimir Ivanov <vladimir.vs.ivanov@gmail.com>
2023-12-03 21:53:51 +0200

    First star

---
+++ adventofcode1.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const getCalibarion = (line) => {
+    const numbers = line.replace(/\D/g, '');
+    const first = numbers[0];
+    const second = numbers[numbers.length-1];
+    return parseInt(first+second);
+
+}
+function main() {
+    const fileStream = fs.createReadStream('input1.txt');
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+    let sum = 0;
+
+    rl.on('line', (line) => {
+        console.log(`Line from file: ${getCalibarion(line)}`);
+        sum += getCalibarion(line);
+    });
+
+    rl.on('close', () => {
+        console.log('sum', sum);
+    })
+}
+
+main();
